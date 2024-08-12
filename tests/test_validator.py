67553f66150702996ff1fd1eabd306eefd2f36b4
timoleistner <leistner.timo@googlemail.com>
2024-08-12 12:19:07 +0200

    Initialize rda-toolbox

---
+++ tests/test_validator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pytest
+import os
+from rda_toolbox.parser import filepaths_to_df
+
+rawfiles_testfolder = "./tests/rawfiles_testfolder"
+rawfile_paths = os.listdir(rawfiles_testfolder)
+
+# def validator():
+#     return
+def test_filepaths_to_filedicts():
+    assert filepaths_to_df(rawfile_paths) == None
