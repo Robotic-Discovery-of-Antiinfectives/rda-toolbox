9d5fea04ec82fd390c49c754c5beac2614e9f080
timoleistner <leistner.timo@googlemail.com>
2024-08-13 17:04:53 +0200

    organize imports

--- rda_toolbox/__init__.py
+++ rda_toolbox/__init__.py
@@ -4,5 +4,6 @@
 # import rda_toolbox as rda
 # rda.readerfiles_to_df()
 
-from .parser import readerfiles_rawdf
-from .parser import readerfiles_metadf
+from .parser import readerfiles_metadf, readerfiles_rawdf
+from .plot import get_plateheatmaps
+from .process import preprocess
