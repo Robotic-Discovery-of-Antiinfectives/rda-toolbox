8481971f2425d862ca240b2dfcb7dbddfa8cd24e
timoleistner <leistner.timo@googlemail.com>
2024-08-19 16:09:26 +0200

    remove type hint of custom marimo type, idk how to import it

--- rda_toolbox/marimo.py
+++ rda_toolbox/marimo.py
@@ -4,9 +4,10 @@ from io import StringIO
 from .parser import readerfile_parser
 from .parser import collect_results
 from .parser import collect_metadata
+# from marimo import FileUploadResults
 
 
-def readeruploads_rawdf(uploadfiles: list[FileUploadResults]):
+def readeruploads_rawdf(uploadfiles):
     """
     Wrapper function to collect the results from marimos file upload button.
     This function is similar to readerfiles_rawdf() except that the input
